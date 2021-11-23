eac04c0bfa12fc8443f7d65a3751168dabf71035
Britodfbr <britodfbr@gmail.com>
2021-11-23 19:52:36 -0300

    implementation without gettext

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 __author__ = "@britodfbr"  # pragma: no cover
+
+"""
+Tutorial disponível em: https://phrase.com/blog/posts/translate-python-gnu-gettext/
+"""
+
+
+# main.py
+def print_some_strings():
+    print("Hello world")
+    print("This is a translatable string")
+
+if __name__ == '__main__':
+    print_some_strings()
